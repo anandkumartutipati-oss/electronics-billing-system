b37c11f04ed12795c011d4aecbb8ef6bb5c9e85b
anandkumartutipati-oss <anandkumartutipati@gmail.com>
2025-12-31 17:54:58 +0530

    bakend production url added in frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 const BASE_URL = import.meta.env.MODE === 'development'
     ? 'http://localhost:5000/api'
-    : 'https://electronics-billing-system.vercel.app/api'; // Production URL for Vercel
+    : 'https://electronics-billing-system-backend.onrender.com/api'; // Production URL for Vercel
 
 export default BASE_URL;
