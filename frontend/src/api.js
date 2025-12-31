a712d73d22e85e4eeb9ec8b92d46dec8605f8944
anandkumartutipati-oss <anandkumartutipati@gmail.com>
2025-12-31 17:40:52 +0530

    some validations are done

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 const BASE_URL = import.meta.env.MODE === 'development'
     ? 'http://localhost:5000/api'
-    : '/api'; // Production URL for Vercel
+    : 'https://electronics-billing-system.vercel.app/api'; // Production URL for Vercel
 
 export default BASE_URL;
