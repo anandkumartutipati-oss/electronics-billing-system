5fec7fc83d9df7daa171c017a4d49f84793be01c
anandkumartutipati-oss <anandkumartutipati@gmail.com>
2026-01-03 18:11:39 +0530

    api.js changed

--- backend/app.js
+++ backend/app.js
@@ -17,8 +17,8 @@ import uploadRoutes from './routes/uploadRoutes.js';
 const app = express();
 
 // Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cors());
 app.use(helmet({
     crossOriginResourcePolicy: { policy: "cross-origin" },
